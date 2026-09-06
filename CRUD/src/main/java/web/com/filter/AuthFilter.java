636f775c02a99655f7bdbc6f6b6dc4e4dadaa93f
tranngoctuyen019-creator <tranngoctuyen019b@gmail.com>
2026-09-06 17:30:26 +0700

    filter

---
+++ CRUD/src/main/java/web/com/filter/AuthFilter.java
@@ -0,0 +1,43 @@
+package web.com.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns = { "/admin/*" })
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false);
+        boolean isLoggedIn = (session != null && session.getAttribute("account") != null);
+
+        if (isLoggedIn) {
+            chain.doFilter(request, response);
+        } else {
+            resp.sendRedirect(req.getContextPath() + "/login");
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
